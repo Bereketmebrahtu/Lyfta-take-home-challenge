a3056afe74db542309a4ad5ad3c2977574fcb188
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-21 13:16:04 +0000

    Replaced video file with a higher resolution file

---
+++ app.js
@@ -0,0 +1,27 @@
+// grab our elements
+
+const playerEl = document.querySelector('.player');
+const videoEl = playerEl.querySelector('.video');
+const progressEl = playerEl.querySelector('.progress');
+const progressBarEl = playerEl.querySelector('.progress-filled');
+const buttonsEl = playerEl.querySelector('.player-button');
+const skipButtonsEl = playerEl.querySelectorAll('.data-skip');
+const rangesEl = playerEl.querySelectorAll('.slider');
+
+function toggleProgress() {
+const playMethod = videoEl.paused ? 'play' : 'paused';
+videoEl[playMethod]()
+
+}
+
+function updateButton() {
+    const icon = this.pause ?  '►' : '❚ ❚';
+    buttonsEl.textContent = icon;
+}
+
+
+videoEl.addEventListener('click', toggleProgress);
+videoEl.addEventListener('click', toggleProgress);
+
+
+buttonsEl.addEventListener('click', toggleProgress);
